211bb06e380b6200e432c09346d14ec71ce81430
Zac <zacharyfarnworth@gmail.com>
2021-04-11 23:45:48 -0600

    fixing deployment issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,11 @@
-const mongoose = require('mongoose');
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-MONGODB_URI = `${process.env.MONGODB_URI}mongodb+srv://WizAdmin:${process.env.DB_PASS}@wizcluster.nzntb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+MONGODB_URI = `${process.env.MONGODB_URI}mongodb+srv://WizAdmin:${process.env.DB_PASS}@wizcluster.nzntb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-mongoose.connect(MONGODB_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: true,
-    }
-);
-
-module.exports = mongoose.connection;
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+});
